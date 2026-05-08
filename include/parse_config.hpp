1e548964af98a7b13ab5f9f82990f2f8d3a2b0e5
crac <crac@defuser>
2026-05-08 17:47:44 +0200

    ajout branche et début parsing

---
+++ include/parse_config.hpp
@@ -0,0 +1,15 @@
+#ifndef PARSE_CONFIG_HPP
+#define PARSE_CONFIG_HPP
+
+class serv_config {
+    int listen_port;
+
+};
+
+/*listenable ports : 0-1023
+                    1024-49151
+                    46152 - 65535
+                    */
+                   
+
+#endif
